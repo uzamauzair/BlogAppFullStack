027166bd4baba469300c9d76adb16c7daca3703b
uzamauzair <usama19026@gmail.com>
2024-03-13 06:54:17 +0530

    Layout Navbar and Footer update

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from "react";
+
+const Footer = () => {
+  console.log("Hello");
+  
+  return (
+    <div>
+      <div>©2024 Bloggero . All rights received.</div>
+    </div>
+  );
+};
+
+export default Footer;
