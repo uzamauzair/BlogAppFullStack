23336a9b1b6300934b49fe7c6f89b76cf77ee062
uzamauzair <usama19026@gmail.com>
2024-03-13 16:37:52 +0530

    Home page developed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "public/hero.png";
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products.</h1>
+        <p className={styles.description}>Turning your idea into Reality, We bring together the teams from the global tech industry.</p>
+        <button className={styles.button}>See our works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="Home page" className={styles.img} />
+      </div>
     </div>
-  )
+  );
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Image from 'next/image';
 import styles from './footer.module.css'
 
 const Footer = () => {
@@ -7,6 +8,12 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div>©2024 Bloggero . All rights received.</div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={25} height={25} className={styles.icon} alt='Bloggero FB' />
+        <Image src="/2.png" width={25} height={25} className={styles.icon} alt='Bloggero Insta ' />
+        <Image src="/3.png" width={25} height={25} className={styles.icon} alt='Bloggero Twitter' />
+        <Image src="/4.png" width={25} height={25} className={styles.icon} alt='Bloggero Youtube' />
+      </div>
     </div>
   );
 };
