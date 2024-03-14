61d764d13ce823517b09c76dd17f9c5637208ef6
uzamauzair <usama19026@gmail.com>
2024-03-14 18:05:32 +0530

    Contact page developed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,24 @@
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import ContactPic from 'public/contact.png'
+import Button from "@/components/button/Button";
 const Contact = () => {
   return (
     <div className={styles.container}>
-      Contact
+      <h1 className={styles.title}>Lets Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src={ContactPic} fill={true} alt="" className={styles.image} />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="Name" className={styles.input} />
+          <input type="text" placeholder="Email" className={styles.input} />
+          <textarea className={styles.textArea} placeholder="Message" cols={30} rows={10}></textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
